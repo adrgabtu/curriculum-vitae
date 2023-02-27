511b4733436d42f83cd0250d5db72c832943cdd4
HGA Team <93742120+adrgabtu@users.noreply.github.com>
2023-02-27 20:00:19 -0300

    Styles and Image removed
    Se eliminaron estilos no utilizados e imagenes

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,4 +1,4 @@
-// VARIABLES GLOBALES
+// VARIABLES
 var full_name = document.getElementById("full_name");
 var pre_name = document.getElementById("pre_name");
 var age = document.getElementById("age");
@@ -9,22 +9,23 @@ var email = document.getElementById("email");
 var img = document.querySelector("img");
 var navbar_brand = document.querySelector("a", "navbar-brand");
 var title = document.querySelector("title");
-
 var div_elements = document.getElementById("exp_list");
-
+// elementos de lista
 const cms = document.getElementById("cms");
 const framework = document.getElementById("framework");
 const dbs = document.getElementById("dbs");
 const dbs_no_sql = document.getElementById("dbs-no-sql");
 
 $(document).ready(function () {
+// elementos de lista inicializan ocultos
   cms.style.display = "none";
   framework.style.display = "none";
   dbs.style.display = "none";
   dbs_no_sql.style.display = "none";
 
   mostrarOcultar();
-  // CUANDO EL DOCUMENTO INICIA, SE CARGA CONTENIDO DE API
+
+  // CUANDO EL DOCUMENTO INICIA, SE CARGA CONTENIDO DE LA API
   $.ajax({
     url: "https://randomuser.me/api/",
     dataType: "json",
@@ -103,39 +104,38 @@ $(document).ready(function () {
       $(".go-top").slideUp(300);
     }
   });
-
-  function mostrarOcultar() {
-    document.querySelectorAll("li.list-item").forEach((element) => {
-      element.addEventListener("click", function (e) {
-        e.preventDefault();
-        let elm_list = e.target.dataset.content;
-
-        if(elm_list == "cms"){
-          cms.style.display = "block";
-        }else{
-          cms.style.display = "none";
-        }
-
-        if(elm_list == "framework"){
-          framework.style.display = "block";
-        }else{
-          framework.style.display = "none";
-        }        
-
-        if (elm_list == "dbs") {
-          dbs.style.display = "block";
-        } else {
-          dbs.style.display = "none";
-        }
-
-        if (elm_list == "dbs-no-sql") {
-          dbs_no_sql.style.display = "block";
-        } else {
-          dbs_no_sql.style.display = "none";
-        }
-
-      });
-    });
-  }
-  
 });
+
+function mostrarOcultar() {
+  document.querySelectorAll("li.list-item").forEach((element) => {
+    element.addEventListener("click", function (e) {
+      e.preventDefault();
+      let elm_list = e.target.dataset.content;
+
+      if (elm_list == "cms") {
+        cms.style.display = "block";
+      } else {
+        cms.style.display = "none";
+      }
+
+      if (elm_list == "framework") {
+        framework.style.display = "block";
+      } else {
+        framework.style.display = "none";
+      }
+
+      if (elm_list == "dbs") {
+        dbs.style.display = "block";
+      } else {
+        dbs.style.display = "none";
+      }
+
+      if (elm_list == "dbs-no-sql") {
+        dbs_no_sql.style.display = "block";
+      } else {
+        dbs_no_sql.style.display = "none";
+      }
+      
+    });
+  });
+}
